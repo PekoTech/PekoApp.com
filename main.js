fd0c58340ac33824f5c15e2a5fb1151d949ef21c
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-23 10:32:25 -0700

    feat: initial page

---
+++ main.js
@@ -0,0 +1,14 @@
+const emailInput = document.getElementById("email");
+const firstNameInput = document.getElementById("first_name");
+const lastNameInput = document.getElementById("last_name");
+const button = document.getElementById("submit");
+
+const url = "";
+
+function submit() {
+  const [email, first_name, last_name] = [
+    emailInput.value,
+    firstNameInput.value,
+    lastNameInput.value,
+  ];
+}
