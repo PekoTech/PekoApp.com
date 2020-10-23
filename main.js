13c64acaa770b7a83ab6b16454ccc2969b7012b2
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-23 10:38:31 -0700

    feat: add submit feature

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const firstNameInput = document.getElementById("first_name");
 const lastNameInput = document.getElementById("last_name");
 const button = document.getElementById("submit");
 
-const url = "";
+const url = "https://peko-egg-email.herokuapp.com/";
+
+const message = document.getElementById("message");
 
 function submit() {
   const [email, first_name, last_name] = [
@@ -11,4 +13,22 @@ function submit() {
     firstNameInput.value,
     lastNameInput.value,
   ];
+
+  message.innerText = "Loading...";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, first_name, last_name }),
+  }).then((res) => {
+    if (res.ok) {
+      message.innerText = "Thanks! We'll be in touch soon.";
+    } else {
+      message.innerText = "Something went wrong while saving your profile.";
+      console.error(res);
+    }
+  });
 }
+
+button.addEventListener("click", submit);
