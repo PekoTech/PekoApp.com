971cec81108b817cc545477e284b88f4dc88dc5f
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-29 22:13:27 -0700

    feat: migrate to next

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Peko App</title>
+      </Head>
+      <main
+        className={
+          "bg-white px-8 py-12 my-0 mx-auto max-w-screen-sm lg:flex lg:max-w-screen-xl"
+        }
+      >
+        Hello!
+      </main>
+    </>
+  );
+}
