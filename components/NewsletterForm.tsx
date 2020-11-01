957f9afb3cc543320fc6d70e8b554446618fd7d8
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-31 18:10:51 -0700

    refactor: move submit form to own component

---
+++ components/NewsletterForm.tsx
@@ -0,0 +1,65 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+import clsx from 'clsx'
+
+import styles from './NewsletterForm.module.scss'
+
+export default function NewsletterForm() {
+  const [form, setForm] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+  })
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    console.log(form) // stub
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name
+        <input
+          className={clsx(styles.input, 'mb-4')}
+          id="first_name"
+          name="first_name"
+          type="text"
+          placeholder="John"
+          value={form.first_name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last Name
+        <input
+          className={clsx(styles.input, 'mb-4')}
+          id="last_name"
+          name="last_name"
+          type="text"
+          placeholder="Smith"
+          value={form.last_name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email
+        <input
+          className={styles.input}
+          id="email"
+          name="email"
+          type="email"
+          placeholder="john@example.com"
+          value={form.email}
+          onChange={handleChange}
+        />
+      </label>
+      <button className={clsx(styles.submit, 'mt-6')} type="submit">
+        Submit
+      </button>
+    </form>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import clsx from 'clsx'
 
 import styles from '../styles/Home.module.scss'
+import NewsletterForm from '../components/NewsletterForm'
 
 export default function Home() {
   return (
@@ -41,34 +41,7 @@ export default function Home() {
             <h1 className="font-bold mb-1">STAY IN THE KNOW.</h1>
             <p>Subsribe for release updates and beta testing.</p>
           </header>
-          <form>
-            <input
-              className={clsx(styles.input, 'mb-4')}
-              id="first_name"
-              type="text"
-              placeholder="First Name"
-            />
-            <input
-              className={clsx(styles.input, 'mb-4')}
-              id="last_name"
-              type="text"
-              placeholder="Last Name"
-            />
-            <input
-              className={styles.input}
-              id="email"
-              type="text"
-              placeholder="Email Address"
-              maxLength={50}
-            />
-            <button
-              className={clsx(styles.submit, 'mt-4')}
-              id="submit"
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
+          <NewsletterForm />
         </section>
       </main>
     </>
