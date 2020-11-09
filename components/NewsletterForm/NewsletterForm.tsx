ab27e4f53d7cb1e35802c23fca16138d017d5026
Kyle Mas <34122775+kylemas@users.noreply.github.com>
2020-11-08 17:05:54 -0800

    feat: Improve Form Loading Indicator (#4)
    * feat: use spinner animation when form is loading
    
    * style: added loader paddings
    
    * fix: place loader inside the 'submit' button, change colors

--- components/NewsletterForm/NewsletterForm.tsx
+++ components/NewsletterForm/NewsletterForm.tsx
@@ -1,5 +1,6 @@
-import { useState, ChangeEvent, FormEvent } from 'react'
 import clsx from 'clsx'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 import styles from './NewsletterForm.module.scss'
 
@@ -94,7 +95,11 @@ export default function NewsletterForm() {
           className={clsx(styles.submit, 'mt-6 lg:col-span-2')}
           type="submit"
         >
-          Submit
+          {state === FormState.Loading ? (
+            <ClipLoader size={25} color={'#ffffff'} />
+          ) : (
+            'Submit'
+          )}
         </button>
       </form>
       <div>
@@ -114,9 +119,6 @@ function Message({ state, error }: MessageProps) {
   switch (state) {
     case FormState.Error:
       message = `Something went wrong while saving your profile. ${error.status}: ${error.message}`
-      break
-    case FormState.Loading:
-      message = 'Loading...'
       break
     case FormState.Done:
       message = "Thanks! We'll be in touch soon."
