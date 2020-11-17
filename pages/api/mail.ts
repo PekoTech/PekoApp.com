127c1575bd92459301fa42f1e801d4556b0c4608
Nanda Syahrasyad <nanda.s@hey.com>
2020-11-16 21:37:06 -0800

    fix: mailing timeout problems (#9)

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,20 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import pg from 'pg'
 
-export const pool = new pg.Pool()
-
 export default async function subscribe(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
     if (req.method === 'POST') {
+      const pool = new pg.Pool()
       const { email, first_name, last_name } = req.body
       await pool.query(
         'INSERT INTO leads(email, first_name, last_name) VALUES($1, $2, $3)',
         [email, first_name, last_name]
       )
-      return res.status(204).send('')
+      await pool.end()
+      return res.status(204).send({ message: 'Thanks!' })
     }
   } catch (error) {
     return res.status(400).send({ error })
