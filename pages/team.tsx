c26ced980758b1e789f8b6b17c8759d2744cfcf0
Narendra Syahrasyad <nanda.s@hey.com>
2020-12-09 16:52:37 -0800

    feat: add linkedin links

--- pages/team.tsx
+++ pages/team.tsx
@@ -9,34 +9,42 @@ const TeamMembers = [
   {
     name: 'Sang Le',
     title: 'CEO & Co-Founder',
+    link: 'https://www.linkedin.com/in/sanglethu/',
   },
   {
     name: 'Sandy Co',
     title: 'Head of Design & Co-Founder',
+    link: 'https://www.linkedin.com/in/sandyklc/',
   },
   {
     name: 'Nanda Syahrasyad',
     title: 'Head of Engineering',
+    link: 'https://www.linkedin.com/in/narendrass/',
   },
   {
     name: 'Najla Sekariyanti',
     title: 'Product Designer',
+    link: 'https://www.linkedin.com/in/najlasekar/',
   },
   {
     name: 'Connor Fong',
     title: 'Technical Advisor',
+    link: 'https://www.linkedin.com/in/connor-fong/',
   },
   {
     name: 'Andrew Zulaybar',
     title: 'Software Engineer',
+    link: 'https://www.linkedin.com/in/andrewzulaybar/',
   },
   {
     name: 'Li Ze Choo',
     title: 'Technical Project Manager',
+    link: 'https://www.linkedin.com/in/lizechoo/',
   },
   {
     name: 'Kyle Mas',
     title: 'Software Engineer',
+    link: 'https://www.linkedin.com/in/kylemas/',
   },
 ] as const
 
@@ -75,28 +83,29 @@ function Avatar({ member }) {
   return (
     // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
     <li
-      className="flex flex-col items-center text-center"
       onMouseOver={() => !hover && setHover(true)}
       onMouseOut={() => hover && setHover(false)}
     >
-      <div
-        role="img"
-        style={{
-          backgroundImage: `url(${path}.JPG)`,
-          display: hover ? 'none' : 'block',
-        }}
-        className="w-32 h-32 bg-gray-500 rounded-full mb-4 md:w-40 md:h-40 bg-cover bg-center"
-      />
-      <div
-        role="img"
-        style={{
-          backgroundImage: `url(${path}_alt.JPG)`,
-          display: hover ? 'block' : 'none',
-        }}
-        className="w-32 h-32 bg-gray-500 rounded-full mb-4 md:w-40 md:h-40 bg-cover bg-center"
-      />
-      <h1 className="font-medium">{member.name}</h1>
-      <p className="text-sm italic">{member.title}</p>
+      <a className="flex flex-col items-center text-center" href={member.link}>
+        <div
+          role="img"
+          style={{
+            backgroundImage: `url(${path}.JPG)`,
+            display: hover ? 'none' : 'block',
+          }}
+          className="w-32 h-32 bg-gray-500 rounded-full mb-4 md:w-40 md:h-40 bg-cover bg-center"
+        />
+        <div
+          role="img"
+          style={{
+            backgroundImage: `url(${path}_alt.JPG)`,
+            display: hover ? 'block' : 'none',
+          }}
+          className="w-32 h-32 bg-gray-500 rounded-full mb-4 md:w-40 md:h-40 bg-cover bg-center"
+        />
+        <h1 className="font-medium">{member.name}</h1>
+        <p className="text-sm italic">{member.title}</p>
+      </a>
     </li>
   )
 }
