f6140f2a862142af8c8bcc80728c23f5e611d4cb
Nanda Syahrasyad <nanda.s@hey.com>
2021-05-06 13:01:00 -0700

    feat: improve landing page design (#14)

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,14 +3,5 @@ import clsx from 'clsx'
 import styles from './Layout.module.scss'
 
 export default function Layout({ children }) {
-  return (
-    <main
-      className={clsx(
-        styles.main,
-        'p-6 py-20 lg:py-24 flex-col lg:px-16 lg:flex lg:items-center'
-      )}
-    >
-      {children}
-    </main>
-  )
+  return <main className={clsx(styles.main)}>{children}</main>
 }
